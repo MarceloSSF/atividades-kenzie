b1f1a7b873c893983e3c87a1e2ae75954eb0cff5
Marcelo Soares <soaresmarcelo94@gmail.com>
2022-02-21 13:16:37 -0300

    update: tabela multiplicacao criada

---
+++ Entregas Sprint 2/TabelaMultiplicacao/index.js
@@ -0,0 +1,24 @@
+function tabelaMultiplicacao(linha, coluna) {
+
+    // Criando novo array com a quantidade linha, do parâmetro da função
+    const tabela = new Array(linha);
+
+    // Criando, para cada linha do array criado, a quantidade coluna, do parâmetro da função
+    for (let i = 0; i <= linha; i++) {
+        tabela[i] = new Array(coluna);
+    }
+
+    // Percorrendo toda a matriz criada e adicionando respectivos valores
+    for (let i = 0; i <= linha; i++) {
+        for (let j=0; j <= coluna; j++) {
+            tabela[i][j] = i + "*" + j + " = " + i*j; // linha * coluna = resultadoo
+        }
+    }
+
+    // Imprimindo tabela completa no console
+    console.table(tabela)
+
+}
+
+// Obs: Os parâmetros indicam os números finais da multiplicação, iniciando em 0 e indo até o valor colocado
+tabelaMultiplicacao(10,10);
